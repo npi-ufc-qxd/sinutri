4a1d5445102e01f3cab56bba99ccdcb63336a626
IsmaelRS <ismaelr.soares@gmail.com>
2014-08-19 17:28:17 -0300

    Criado Classe Paciente, iniciado página de cadastro do Paciente

---
+++ aplicacoes/sisat/src/main/java/br/com/ufc/quixada/npi/sisat/model/Paciente.java
@@ -0,0 +1,46 @@
+package br.com.ufc.quixada.npi.sisat.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Paciente implements Serializable {
+	
+	
+	@Id
+	@OneToOne
+    @JoinColumn(name="id", nullable=false)
+    private Pessoa pessoa;
+	
+	private double altura;
+
+	
+	/*
+	 //
+	@OneToMany(mappedBy="paciente")
+	private List<ConsultaNutricional> consultas;
+	
+	@OneToMany(mappedBy="paciente")
+	private List<Agendamento> agendamentos;
+	 */	
+	
+	
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	public double getAltura() {
+		return altura;
+	}
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	} 
+}
