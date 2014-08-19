467e7c42b3e4251f5363d2e404a97b9f408a1a2e
Wanrly <wanrly.sousa@hotmail.com>
2014-08-19 14:04:35 -0300

    Foi criada a classe ConsultaNutricional e a declaração das variáveis.

--- aplicacoes/sisat/src/main/java/br/com/ufc/quixada/npi/sisat/model/ConsultaNutricional.java
+++ aplicacoes/sisat/src/main/java/br/com/ufc/quixada/npi/sisat/model/ConsultaNutricional.java
@@ -69,6 +69,214 @@ public class ConsultaNutricional {
 	private Boolean bebidaAlcoolica;
 	
 	private int bebidaAlcoolicaFrequencia;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getCircunferenciaCintura() {
+		return circunferenciaCintura;
+	}
+
+	public void setCircunferenciaCintura(Double circunferenciaCintura) {
+		this.circunferenciaCintura = circunferenciaCintura;
+	}
+
+	public int getGlicemia() {
+		return glicemia;
+	}
+
+	public void setGlicemia(int glicemia) {
+		this.glicemia = glicemia;
+	}
+
+	public int getTgl() {
+		return tgl;
+	}
+
+	public void setTgl(int tgl) {
+		this.tgl = tgl;
+	}
+
+	public int getHdl() {
+		return hdl;
+	}
+
+	public void setHdl(int hdl) {
+		this.hdl = hdl;
+	}
+
+	public int getIdl() {
+		return idl;
+	}
+
+	public void setIdl(int idl) {
+		this.idl = idl;
+	}
+
+	public int getHt() {
+		return ht;
+	}
+
+	public void setHt(int ht) {
+		this.ht = ht;
+	}
+
+	public int getHb() {
+		return hb;
+	}
+
+	public void setHb(int hb) {
+		this.hb = hb;
+	}
+
+	public String getCondutaNutricional() {
+		return condutaNutricional;
+	}
+
+	public void setCondutaNutricional(String condutaNutricional) {
+		this.condutaNutricional = condutaNutricional;
+	}
+
+	public Boolean getMedicamento() {
+		return medicamento;
+	}
+
+	public void setMedicamento(Boolean medicamento) {
+		this.medicamento = medicamento;
+	}
+
+	public String getMedicamentoComentario() {
+		return medicamentoComentario;
+	}
+
+	public void setMedicamentoComentario(String medicamentoComentario) {
+		this.medicamentoComentario = medicamentoComentario;
+	}
+
+	public Boolean getMastigacao() {
+		return mastigacao;
+	}
+
+	public void setMastigacao(Boolean mastigacao) {
+		this.mastigacao = mastigacao;
+	}
+
+	public String getMastigacaoComentario() {
+		return mastigacaoComentario;
+	}
+
+	public void setMastigacaoComentario(String mastigacaoComentario) {
+		this.mastigacaoComentario = mastigacaoComentario;
+	}
+
+	public Boolean getDisfagia() {
+		return disfagia;
+	}
+
+	public void setDisfagia(Boolean disfagia) {
+		this.disfagia = disfagia;
+	}
+
+	public Boolean getPirose() {
+		return pirose;
+	}
+
+	public void setPirose(Boolean pirose) {
+		this.pirose = pirose;
+	}
+
+	public Boolean getNausea() {
+		return nausea;
+	}
+
+	public void setNausea(Boolean nausea) {
+		this.nausea = nausea;
+	}
+
+	public Boolean getVomito() {
+		return vomito;
+	}
+
+	public void setVomito(Boolean vomito) {
+		this.vomito = vomito;
+	}
+
+	public Boolean getDiarreia() {
+		return diarreia;
+	}
+
+	public void setDiarreia(Boolean diarreia) {
+		this.diarreia = diarreia;
+	}
+
+	public Boolean getConstipacao() {
+		return constipacao;
+	}
+
+	public void setConstipacao(Boolean constipacao) {
+		this.constipacao = constipacao;
+	}
+
+	public String getAgua() {
+		return agua;
+	}
+
+	public void setAgua(String agua) {
+		this.agua = agua;
+	}
+
+	public String getCarneVermelhaFrequencia() {
+		return carneVermelhaFrequencia;
+	}
+
+	public void setCarneVermelhaFrequencia(String carneVermelhaFrequencia) {
+		this.carneVermelhaFrequencia = carneVermelhaFrequencia;
+	}
+
+	public Boolean getBebidaAlcoolica() {
+		return bebidaAlcoolica;
+	}
+
+	public void setBebidaAlcoolica(Boolean bebidaAlcoolica) {
+		this.bebidaAlcoolica = bebidaAlcoolica;
+	}
+
+	public int getBebidaAlcoolicaFrequencia() {
+		return bebidaAlcoolicaFrequencia;
+	}
+
+	public void setBebidaAlcoolicaFrequencia(int bebidaAlcoolicaFrequencia) {
+		this.bebidaAlcoolicaFrequencia = bebidaAlcoolicaFrequencia;
+	}
 	
 	
 	
