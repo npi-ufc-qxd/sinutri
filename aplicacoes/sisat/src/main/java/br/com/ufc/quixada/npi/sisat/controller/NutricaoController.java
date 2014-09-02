2755ea8c0c2da0944e6ee74197395c219b5701bf
IsmaelRS <ismaelr.soares@gmail.com>
2014-09-02 17:20:11 -0300

    Adicionados os detalhes dos pacientes. Busca os pacientes na tabela de pessoas.

--- aplicacoes/sisat/src/main/java/br/com/ufc/quixada/npi/sisat/controller/NutricaoController.java
+++ aplicacoes/sisat/src/main/java/br/com/ufc/quixada/npi/sisat/controller/NutricaoController.java
@@ -9,10 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.ufc.quixada.npi.sisat.model.Paciente;
 import br.com.ufc.quixada.npi.sisat.model.Pessoa;
 import br.com.ufc.quixada.npi.sisat.service.PacienteService;
 import br.com.ufc.quixada.npi.sisat.service.PessoaService;
@@ -31,36 +32,48 @@ public class NutricaoController {
 	public String index() {
 		return "nutricao/listar";
 	}
+	
+	//ismael - retorna a primeira página do sisat
+	@RequestMapping(value = {"/buscar"}, method = RequestMethod.GET)
+	public String buscarPaciente(Model model) {
+		model.addAttribute("pessoa", new Pessoa());
+//		model.addAttribute("tipoBusca", new String());
+//		model.addAttribute("busca", new String());
+		
+		return "nutricao/buscar";
+	}
+	
+	//ismael - retorna a primeira página do sisat com uma lista de pessoas
+	@RequestMapping(value = "/buscar", method = RequestMethod.POST)
+	public String buscarPaciente(@Valid @ModelAttribute("pessoa") Pessoa pessoa, BindingResult result, HttpSession session, ModelMap map) {
+		if(!pessoa.getCpf().isEmpty()){
+			map.addAttribute("pessoas", servicePessoa.getPessoasByCpf(pessoa.getCpf()));
+		}else if(!pessoa.getNome().isEmpty()){
+			map.addAttribute("pessoas", servicePessoa.getPessoasByNome(pessoa.getNome()));
+		}
+		return "/nutricao/buscar";
+	}
+	
+	//ismael - detalhes do paciente
+	@RequestMapping(value = {"/{id}/detalhes"})
+	public String getDetalhes(Pessoa p, @PathVariable("id") Long id,
+			Model model, RedirectAttributes redirectAttributes){
+		Pessoa pessoa = servicePessoa.find(Pessoa.class, id);
+		System.out.println("estou aqui!!!");
+		if(pessoa == null){
+			redirectAttributes.addFlashAttribute("erro", "Paciente não encontrado.");
+			return "redirect:/nutricao/buscar";
+		}
+		System.out.println(pessoa.getNome());
+		model.addAttribute("pessoa", pessoa);
+		return "nutricao/detalhes";
+	}
 
 
 	@RequestMapping(value = {"/consulta"}, method = RequestMethod.GET)
 	public String consulta(Model model) {
 		System.out.println("consulta");
 		return "nutricao/consulta";
-	}
-
-	//Cadastrar paciente
-	@RequestMapping(value = "/paciente/cadastrar", method = RequestMethod.POST)
-	public String adicionarPaciente(@Valid @ModelAttribute("paciente") Paciente paciente, BindingResult result, HttpSession session, ModelMap map) {
-		System.out.println("aqui o/");
-		
-		if (result.hasErrors()) {
-			return ("/paciente/cadastrar");
-		}
-		Pessoa pessoa = servicePessoa.getPessoaByCpf(paciente.getPessoa().getCpf());	//verifica se a pessoa já existe
-		if(pessoa == null){
-			System.out.println("ERROR!");
-			map.addAttribute("erro", "Paciente não encontrado!!!");
-		}else{
-			System.out.println("OK ;)");
-			paciente.setPessoa(pessoa);
-			this.servicePaciente.save(paciente);
-
-			map.addAttribute("info", "Paciente cadastrado com sucesso.");
-		}
-		//return "redirect:/nutricao/paciente/listar";		//ainda não existe essa view
-		
-		return "/nutricao/paciente/cadastrar";
 	}
 	
 	@RequestMapping(value = "/{id}/mostrar")
