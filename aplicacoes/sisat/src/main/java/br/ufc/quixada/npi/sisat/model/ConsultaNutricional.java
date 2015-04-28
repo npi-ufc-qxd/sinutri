f19a69b3d384fb59938315e8873e45cbc49d301e
matheussouzas <matheussouza.inf@gmail.com>
2015-04-28 15:52:45 -0300

    Campos Altura, Peso e CC não aceitam valor 0.

--- aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/model/ConsultaNutricional.java
+++ aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/model/ConsultaNutricional.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -39,9 +40,11 @@ public class ConsultaNutricional {
 	private Date data;
 
 	@NotNull(message = "Por favor, informe o peso do paciente!")
+	@Min (value = 1)
 	private Double peso;
 
 	@NotNull(message = "Por favor, informe a cincunferencia da cintura do paciente!")
+	@Min (value = 1)
 	private Double circunferenciaCintura;
 
 	private Integer glicemia;
