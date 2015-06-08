f9c95db5555604c2a5245b15c4ecca93f86b4c74
guilhermeestevao <guilhermeestevo@gmail.com>
2015-06-08 17:16:36 -0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/npi-ufc-qxd/sisat-sistema-de-atendimento into
    1556_Adição_da_alimentação_na_frequencia_alimentar
    
    Conflicts:
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/controller/NutricaoController.java
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/model/ConsultaNutricional.java
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/model/Paciente.java
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/model/Pessoa.java
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/service/ConsultaNutricionalService.java
    	aplicacoes/sisat/src/main/java/br/ufc/quixada/npi/sisat/service/impl/ConsultaNutricionalServiceImpl.java
    	aplicacoes/sisat/src/main/webapp/WEB-INF/view/nutricao/consulta.jsp

--- aplicacoes/sisat/src/main/webapp/resources/js/questionario-frequencia-alimentar.js
+++ aplicacoes/sisat/src/main/webapp/resources/js/questionario-frequencia-alimentar.js
@@ -4,135 +4,139 @@ $.ajax({
 		data: {"id": id},
 		url: '/sisat/nutricao/frequencia-alimentar.json',
 		success: function(result) {
-			$.each( result, function( key, frequenciaAlimentar ) {
-				console.log("refeição : "+frequenciaAlimentar.refeicao);
-				console.log("Horário : "+frequenciaAlimentar.horario);
-				
-				$.each( frequenciaAlimentar.alimentos, function( key, alimento ) {
-					console.log(alimento.alimento + ' - ' + alimento.porcao);
-				});
-				
-			});
+			loadQuestionarios(result);
 		},
 		error: function(error) {
 			
 		}
 		
 });
-/*	
-success(function(result) {
-		
-		$.each( result, function( key, frequenciaAlimentar ) {
 
-			
 
-			$('#questionarioFrequenciaAlimentar').appendGrid('insertRow', [{
-				'refeicao' : frequenciaAlimentar.refeicao,
-				'horario': frequenciaAlimentar.horario,
-				
-			} ,], frequenciaAlimentar.id);
+function loadQuestionarios(result) {
+	var i = 0;
+	var frequencias = [];
 
-//			$.each( frequenciaAlimentar.alimentos, function( key, alimento ) {
-//				console.log(alimento.alimento + ' - ' + alimento.porcao);
-//			});
-			
-			
-//			$('#questionarioFrequenciaAlimentar').appendGrid('insertRow', [{
-//			'refeicao' : frequenciaAlimentar.refeicao,
-//			'horario': frequenciaAlimentar.horario,
-//			
-//		} ,], frequenciaAlimentar.id);
-		
-	
-	},
-	error(function(error) {
-		$.each( error, function( key, value ) {
-			console.log(key +"--"+value);
+	$.each( result, function( key, frequenciaAlimentar ) {
+		var alimentos = [];
+		var y = 0;
+
+		$.each( frequenciaAlimentar.alimentos, function( key, alimento ) {
+			alimentos[y] = { 
+				'id': alimento.id,
+				'alimento': alimento.alimento, 
+				'porcao': alimento.porcao 
+			};
+			y++;
 		});
-	}
-*/
+
+		frequencias[i] = {
+				'id' : frequenciaAlimentar.id,
+            	'refeicao': frequenciaAlimentar.refeicao, 
+             	'horario': frequenciaAlimentar.horario,
+             	'SubGridData' : alimentos,
+   	   };
+	   i++;
+	});
+
+	$('#questionarioFrequenciaAlimentar').appendGrid('load', frequencias);
+}
+
 
 $(function() {
-		// Initialize appendGrid
-		$('#questionarioFrequenciaAlimentar')
-				.appendGrid(
+	
+	$('#questionarioFrequenciaAlimentar')
+			.appendGrid(
+					{
+						caption : 'Refeições',
+						initRows : 0,
+						maxRowsAllowed : 6,
+						columns : [{
+							name : 'id',
+							type : 'hidden',
+						}, 
+						  
 						{
-							caption : 'Refeições',
-							initRows : 0,
-							maxRowsAllowed : 6,
-							columns : [ {
-								name : 'refeicao',
-								display : 'Tipo de refição',
-								type : 'select',
-								ctrlOptions : 'DESJEJUM:Desjejum;LANCHE_DA_MANHA:Lanche da Manhã;ALMOCO:Almoço;LANCHE_DA_TARDE:Lanche da Tarde;JANTAR:Jantar;CEIA:Ceia'
-										
-							}, {
-								name : 'horario',
-								display : 'Hora',
-								type : 'text',
-								ctrlAttr : {
-									maxlength : 15
-								},
-								ctrlCss : {
-									width : '70px',
-									'text-align' : 'right'
-								},
-								ctrlClass : 'hora'
-							} ],
-							i18n : {
-								rowEmpty : "Nenhum alimentação foi adicionada!"
+							name : 'refeicao',
+							display : 'Tipo de refição',
+							type : 'select',
+							ctrlOptions : 'DESJEJUM:Desjejum;LANCHE_DA_MANHA:Lanche da Manhã;ALMOCO:Almoço;LANCHE_DA_TARDE:Lanche da Tarde;JANTAR:Jantar;CEIA:Ceia'
+									
+						}, {
+							name : 'horario',
+							display : 'Hora',
+							type : 'text',
+							ctrlAttr : {
+								maxlength : 15
 							},
-							nameFormatter : function(idPrefix, name, uniqueIndex) {
-								return "frequencias[" + (uniqueIndex - 1)+ "]." + name;
+							ctrlCss : {
+								width : '70px',
+								'text-align' : 'right'
 							},
-							useSubPanel : true,
-							subPanelBuilder : function(cell, uniqueIndex) {
-								var idPanel = uniqueIndex-1;
-
-								$(".hora").mask("99:99:99");
-				
-								var subgrid = $('<table></table>').attr('id',
-										'tblSubGrid_' + uniqueIndex).appendTo(cell);
-		
-								subgrid.addClass('alternate');
+							ctrlClass : 'hora'
+						} ],
+					
 						
-								subgrid.appendGrid({
-											initRows : 0,
-											hideRowNumColumn : true,
-											columns : [ {
-												name : 'alimento',
-												display : 'Alimento',
-												ctrlCss : {
-													'width' : '200px'
-												}
-											}, {
-												name : 'porcao',
-												display : 'Porção',
-												ctrlCss : {
-													'width' : '100px',
-													'text-align' : 'right'
-												},
+						i18n : {
+							rowEmpty : "Nenhum alimentação foi adicionada!"
+						},
+						nameFormatter : function(idPrefix, name, uniqueIndex) {
+							return "frequencias[" + (uniqueIndex - 1)+ "]." + name;
+						},
+						useSubPanel : true,
+						subPanelBuilder : function(cell, uniqueIndex) {
+							var idPanel = uniqueIndex-1;
 
-											} ],
-											i18n : {
-												rowEmpty : "Nenhum alimento foi adicionado!"
+							$(".hora").mask("99:99:99");
+			
+							var subgrid = $('<table></table>').attr('id',
+									'tblSubGrid_' + uniqueIndex).appendTo(cell);
+	
+							subgrid.addClass('alternate');
+					
+							subgrid.appendGrid({
+										initRows : 0,
+										hideRowNumColumn : true,
+										columns : [{
+											name : 'id',
+											type : 'hidden',
+										},          
+										{
+											name : 'alimento',
+											display : 'Alimento',
+											ctrlCss : {
+												'width' : '200px'
+											}
+										}, {
+											name : 'porcao',
+											display : 'Porção',
+											ctrlCss : {
+												'width' : '100px',
+												'text-align' : 'right'
 											},
-											nameFormatter : function(idPrefix, name, uniqueIndex) {
-												return "frequencias["+ (idPanel)+ "].alimentos["+ (uniqueIndex - 1)+ "]." + name;
-											},
-										});
-							},
-							subPanelGetter : function(uniqueIndex) {
-								// Return the sub grid value inside sub panel for `getAllValue` and `getRowValue` methods
-								return $('#tblSubGrid_' + uniqueIndex).appendGrid('getAllValue', true);
-							},
-							rowDataLoaded : function(caller, record, rowIndex, uniqueIndex) {
-								// Check SubGridData exist in the record data
-								if (record.SubGridData) {
-									// Fill the sub grid
-									$('#tblSubGrid_' + uniqueIndex, caller).appendGrid('load',record.SubGridData);
-								}
+
+										} ],
+										i18n : {
+											rowEmpty : "Nenhum alimento foi adicionado!"
+										},
+										nameFormatter : function(idPrefix, name, uniqueIndex) {
+											return "frequencias["+ (idPanel)+ "].alimentos["+ (uniqueIndex - 1)+ "]." + name;
+										},
+									});
+						},
+						subPanelGetter : function(uniqueIndex) {
+							// Return the sub grid value inside sub panel for `getAllValue` and `getRowValue` methods
+							return $('#tblSubGrid_' + uniqueIndex).appendGrid('getAllValue', true);
+						},
+						rowDataLoaded : function(caller, record, rowIndex, uniqueIndex) {
+							// Check SubGridData exist in the record data
+							if (record.SubGridData) {
+								// Fill the sub grid
+								$('#tblSubGrid_' + uniqueIndex, caller).appendGrid('load',record.SubGridData);
 							}
-						});
+						}
+					});
 
-	});
+});
+
+
