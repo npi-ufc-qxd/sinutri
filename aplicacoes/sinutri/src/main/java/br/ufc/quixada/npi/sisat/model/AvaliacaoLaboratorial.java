648c757b1f4d0ce4fd500d1c6df69dfc3735e3ed
FelipePinhoUFC <felipe76857685@gmail.com>
2016-04-25 17:17:27 -0300

    Feitas correções

--- aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sisat/model/AvaliacaoLaboratorial.java
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sisat/model/AvaliacaoLaboratorial.java
@@ -1,6 +1,7 @@
 package br.ufc.quixada.npi.sisat.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -19,16 +22,62 @@ public class AvaliacaoLaboratorial {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private Date criadoEm;
 	
+	@NotNull
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private Date atualizado;
 	
+	@NotNull
 	private String observacao;
 	
 	@OneToOne
-	@JoinColumn(name = "servidor_id")
 	private Servidor nutricionista;
+	
+	@OneToMany
+	@JoinColumn(name = "exame_id")
+	private List<Exame> exames;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCriadoEm() {
+		return criadoEm;
+	}
+
+	public void setCriadoEm(Date criadoEm) {
+		this.criadoEm = criadoEm;
+	}
+
+	public Date getAtualizado() {
+		return atualizado;
+	}
+
+	public void setAtualizado(Date atualizado) {
+		this.atualizado = atualizado;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public Servidor getNutricionista() {
+		return nutricionista;
+	}
+
+	public void setNutricionista(Servidor nutricionista) {
+		this.nutricionista = nutricionista;
+	}
 	
 }
--- aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sisat/model/Exame.java
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sisat/model/Exame.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Exame {
@@ -12,8 +15,34 @@ public class Exame {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull
 	private br.ufc.quixada.npi.sisat.model.enuns.Exame nome;
 	
+	@NotNull
 	private Double resultado;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public br.ufc.quixada.npi.sisat.model.enuns.Exame getNome() {
+		return nome;
+	}
+
+	public void setNome(br.ufc.quixada.npi.sisat.model.enuns.Exame nome) {
+		this.nome = nome;
+	}
+
+	public Double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+	
 }
