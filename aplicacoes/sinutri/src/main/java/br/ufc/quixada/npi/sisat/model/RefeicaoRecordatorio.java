eedb22d3bc49d70a1aa312a7a28f69c729a72ad3
lucashenriquejbe <lucashenriquejbe@gmail.com>
2016-04-25 14:50:55 -0300

    Criação da classe RefeicaoRecordatorio e alteração da classe Recordatorio.

---
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sisat/model/RefeicaoRecordatorio.java
@@ -0,0 +1,124 @@
+package br.ufc.quixada.npi.sisat.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.ufc.quixada.npi.sisat.model.enuns.Refeicao;
+
+@Entity
+public class RefeicaoRecordatorio {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotNull
+	@DateTimeFormat(pattern="HH:mm")
+	private Date hora;
+	
+	private Refeicao descricao;
+	
+	private String itens;
+	
+	private String observacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getHora() {
+		return hora;
+	}
+
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+
+	public Refeicao getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(Refeicao descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getItens() {
+		return itens;
+	}
+
+	public void setItens(String itens) {
+		this.itens = itens;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itens == null) ? 0 : itens.hashCode());
+		result = prime * result + ((observacao == null) ? 0 : observacao.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RefeicaoRecordatorio other = (RefeicaoRecordatorio) obj;
+		if (descricao != other.descricao)
+			return false;
+		if (hora == null) {
+			if (other.hora != null)
+				return false;
+		} else if (!hora.equals(other.hora))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itens == null) {
+			if (other.itens != null)
+				return false;
+		} else if (!itens.equals(other.itens))
+			return false;
+		if (observacao == null) {
+			if (other.observacao != null)
+				return false;
+		} else if (!observacao.equals(other.observacao))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RefeicaoRecordatorio [id=" + id + ", hora=" + hora + ", descricao=" + descricao + ", itens=" + itens
+				+ ", observacao=" + observacao + "]";
+	}
+	
+	
+
+}
