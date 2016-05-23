4516f30f1c5f2ffb9ca84adf3fcbc26b0a573816
kerley <kerleysol@gmail.com>
2016-05-23 16:00:57 -0300

    Correção de erro do pacienteServiceImpl; Atividade em andamento.

--- aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/service/impl/PacienteServiceImpl.java
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/service/impl/PacienteServiceImpl.java
@@ -1,11 +1,13 @@
 package br.ufc.quixada.npi.sinutri.service.impl;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import br.ufc.quixada.npi.sinutri.model.Paciente;
 import br.ufc.quixada.npi.sinutri.repository.PacienteRepository;
 import br.ufc.quixada.npi.sinutri.service.PacienteService;
 
+@Named
 public class PacienteServiceImpl implements PacienteService{
 
 	@Inject
