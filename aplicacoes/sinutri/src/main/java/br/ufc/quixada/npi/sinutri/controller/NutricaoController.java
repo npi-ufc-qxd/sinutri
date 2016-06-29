64fddc8fadd48f564f60d7579966956e021e3442
sergiofilho07 <sergiofilhoce07@gmail.com>
2016-06-29 14:52:53 -0300

    correção pull request

--- aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/controller/NutricaoController.java
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/controller/NutricaoController.java
@@ -1,17 +1,11 @@
 package br.ufc.quixada.npi.sinutri.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import br.ufc.quixada.npi.ldap.model.Usuario;
+import org.springframework.web.bind.annotation.RequestParam;
 import br.ufc.quixada.npi.ldap.service.UsuarioService;
 
 @Controller
@@ -22,18 +16,14 @@ public class NutricaoController {
 
 	@RequestMapping(value = {"", "/", "/Buscar"}, method = RequestMethod.GET)
 	public String paginaInicial(Model model) {
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		model.addAttribute("usuarios",usuarios);
 		model.addAttribute("ultimaBusca", "");
 		return "nutricao/buscar";
 	}
 	
 	@RequestMapping(value="/Buscar", method = RequestMethod.POST)
-	public String PaginaInicial(Model model, HttpServletRequest request){		
-		String busca = request.getParameter("busca");
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios = usuarioService.getByCpfOrNome(busca);
-		model.addAttribute("usuarios",usuarios);
+	public String paginaInicial(Model model, @RequestParam("busca") String busca){		
+		
+		model.addAttribute("usuarios", usuarioService.getByCpfOrNome(busca));		
 		model.addAttribute("ultimaBusca", busca);
 
 		return "nutricao/buscar";
--- aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/controller/PacienteController.java
+++ aplicacoes/sinutri/src/main/java/br/ufc/quixada/npi/sinutri/controller/PacienteController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
