e99dcb33ba8ca29335e17f31c5a281bb6e5511c5
andredavys <adavys06@gmail.com>
2016-09-13 14:51:18 -0300

    commit para Cainnã

--- aplicacoes/sinutri/src/main/resources/static/js/calculo-energetico/cadastrar.js
+++ aplicacoes/sinutri/src/main/resources/static/js/calculo-energetico/cadastrar.js
@@ -1,15 +1,44 @@
 $(document).ready(function() {
-	$("#inputPesoDesejado").on('keyup', function(){			
-		$("#inputVet").val($("#inputPesoDesejado").val()*30);
+	$("#inputPesoDesejado").on('keyup', function(){	
+		var valorCampo = $("#inputPesoDesejado").val()*30;
+		updateValue("#inputVet", valorCampo);
+		
+		var vet = Number($("#inputVet").val());
+		var vetAjuste = Number($("#inputVetAjuste").val());
+		updateValue("#inputVetReducao", ((vetAjuste*vet)+vet));
 	});
 	
 	$("#inputVetAjuste").on('keyup', function(){
-		var vet = $("#inputVet").val();
-		var vetAjuste = $("#inputVetAjuste").val();
-		console.log("");
-		console.log(vet);
-		console.log(vetAjuste);
-		$("#inputVetReducao").val((vetAjuste*vet));
+		var vet = Number($("#inputVet").val());
+		var vetAjuste = Number($("#inputVetAjuste").val());
+		var valorCampo = (vetAjuste*vet)+vet;
+		updateValue("#inputVetReducao", valorCampo);
+	});
+	
+	$("#inputGlicidio").on('keyup', function(){
+		var ultimoDigito = $("#inputGlicidio").val().slice(0,-1);
+		console.log(ultimoDigito);
+		
+		updateValue("#inputTotal", getInputTotal());
+	});
+	
+	$("#inputProteina").on('keyup', function(){
+		updateValue("#inputTotal", getInputTotal());
+	});
+	
+	$("#inputLipidio").on('keyup', function(){		
+		updateValue("#inputTotal", getInputTotal());
 	});
 	
 });
+
+function updateValue(nomeCampo, valor){
+	$(nomeCampo).val(valor);
+}
+
+function getInputTotal(){
+	var valorGlicidio = Number($("#inputGlicidio").val());
+	var valorProteina = Number($("#inputProteina").val());
+	var valorLipidio = Number($("#inputLipidio").val());
+	return valorGlicidio+valorLipidio+valorProteina;
+}
